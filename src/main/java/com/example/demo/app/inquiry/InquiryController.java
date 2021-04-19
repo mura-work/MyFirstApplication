2bddd46646aff7085bcc4dcc5f13b9d72cced8a6
murata <murata.naoya.infratop@gmail.com>
2021-04-19 20:45:40 +0900

    formの作成まで

--- src/main/java/com/example/demo/app/inquiry/InquiryController.java
+++ src/main/java/com/example/demo/app/inquiry/InquiryController.java
@@ -2,16 +2,54 @@ package com.example.demo.app.inquiry;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/inquiry")
 public class InquiryController {
 	
-	@GetMapping("/form")
-	public String form(Model model) {
+	@GetMapping("/form") // getでリクエストした場合
+	public String form(InquiryForm InquiryForm, 
+			Model model,
+			@ModelAttribute("complete") String complete) {
+		model.addAttribute("title", "Inquiry Form");
+		return "inquiry/form";
+	}
+	
+	@PostMapping("/form") // postでリクエストした場合
+	public String forGoBack(InquiryForm InquiryForm, Model model) {
 		model.addAttribute("title", "Inquiry Form");
 		return "Inquiry/form";
+	}
+	
+	@PostMapping("/confirm") // postでリクエストした場合
+	public String confirm(@Validated InquiryForm inquiryform,
+			BindingResult result,
+			Model model) {
+		if (result.hasErrors()) {
+			model.addAttribute("title", "Inquiry Form");
+			return "inquiry/form";
+		}
+		model.addAttribute("title", "Confirm Page");
+		return "inquiry/confirm";
+	}
+	
+	@PostMapping("/complete")
+	public String complete(@Validated InquiryForm inquiryform, 
+			BindingResult result,
+			Model model,
+			RedirectAttributes redirectAttributes) {
+		if (result.hasErrors()) {
+			model.addAttribute("title", "InquiryForm");
+			return "inquiry/form";
+		}
+		redirectAttributes.addFlashAttribute("complete", "Registered!");
+		return "redirect:/inquiry/form";
 	}
 }
